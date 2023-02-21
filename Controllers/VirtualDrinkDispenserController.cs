f48b4b50b1da95b66ee4be2ea4fb74c2ad12eee5
Nadir Abrar <nabrar@avanceon.ae>
2023-02-22 02:36:56 +0500

    Add project files.

---
+++ Controllers/VirtualDrinkDispenserController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Virtual_Drinks_Dispenser.Model;
+
+namespace Virtual_Drinks_Dispenser.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VirtualDrinkDispenserController : ControllerBase
+    {
+        private readonly object _lockObj = new object();
+        private readonly List<OrderModel> orders = new List<OrderModel>();
+        private readonly int cokesCapacity = 2;
+        private readonly int fantaCpacity = 1;
+        private readonly int preparationTimeInSec = 10;
+        public VirtualDrinkDispenserController()
+        {
+
+        }
+        [Route("OrderDrink")]
+        [HttpPost]
+        public ActionResult OrderDrink(string customerNumber,DrinkType drinkType)
+        {
+            var OrderDetail = new OrderModel(customerNumber,drinkType);
+
+            lock (_lockObj)
+            {
+                if (checkOrderAcceptance(OrderDetail))
+                {
+                    OrderPreparation(OrderDetail);
+                    return StatusCode(200);
+                }
+                else
+                {
+                    return StatusCode(429);
+                }
+            }
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<OrderModel>> GetAllOrdersList()
+        {
+            return orders.ToList();
+        }
+        public bool checkOrderAcceptance(OrderModel order)
+        {
+            if (order.drinkType == DrinkType.Coke)
+            {
+                return orders.Where(x=>x.drinkType== DrinkType.Coke && x.OrderState == OrderState.Serving).ToList().Count< cokesCapacity;
+            }
+            else
+            {
+                return orders.Where(x => x.drinkType ==DrinkType.Fanta && x.OrderState == OrderState.Serving).ToList().Count < fantaCpacity;
+            }
+        }
+        private void OrderPreparation(OrderModel order)
+        {
+            orders.Add(order);
+            ThreadPool.QueueUserWorkItem(_ => ServeDrink(order));
+        }
+        private void ServeDrink(OrderModel order)
+        {
+            Thread.Sleep(preparationTimeInSec * 1000);
+            lock (_lockObj)
+            {
+                var getOrderInfo = orders.Where(x => x.Id == order.Id).FirstOrDefault();
+                if (getOrderInfo!=null)
+                getOrderInfo.OrderState = OrderState.Served;
+            }
+        }
+    }
+}
---
+++ Model/OrderModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Virtual_Drinks_Dispenser.Model
+{
+    public class OrderModel
+    {
+        public string Id { get; } = Guid.NewGuid().ToString();
+        public string? customerNumber { get; }
+        public DrinkType drinkType { get; }
+        public OrderState OrderState { get; set; } = OrderState.Serving;
+        public OrderModel(string CustomerNumber, DrinkType DrinkType)
+        {
+            customerNumber = CustomerNumber;
+            drinkType = DrinkType;
+        }
+    }
+    public enum DrinkType
+    {
+        Coke,
+        Fanta
+    }
+    public enum OrderState
+    {
+        Serving,
+        Served
+    }
+
+}
